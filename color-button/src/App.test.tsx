a2ba0fd76e16d52281d618e7a6718a9dd6eb1c6d
CarlosVinicius3258 <carlosvinicius3258@outlook.com>
2023-05-29 19:51:03 -0300

    add(tests): set initial test scripts

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('button has correct initial color', () => {
   render(<App />);
-  const linkElement = screen.getByRole('link', {name: /learn react/i});
-  expect(linkElement).toBeInTheDocument();
+  const button = screen.getByRole('button', {name: 'Change to blue'})
+
+  expect(button).toHaveStyle({backgorundColor: 'red'})
+})
+
+test('button has correct initial text', () => {
+  render(<App />)
+  const button = screen.getByRole('buton', { name: 'Change to blue' })
+  expect(button.textContent).toBe('Change to blue')
 });
+
+test('button turns blue when clicked', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: 'Change to blue' })
+  button.click()
+  expect(button).toHaveStyle({backgroundColor: 'blue'})
+});
+
+test('button change text when clicked', () => {
+  render(<App />);
+  const button = screen.getByRole('button', { name: "Change to red" })
+  expect(button.textContent).toBe("Change to red")
+})
