673f304f5abd2d6a4ce9c2cc9c7b95784a3d288c
CarlosVinicius3258 <carlosvinicius3258@outlook.com>
2023-05-29 23:50:03 -0300

    add test case for App

--- color-button/src/App.test.tsx
+++ color-button/src/App.test.tsx
@@ -1,28 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('button has correct initial color', () => {
+test('button change on click', () => {
   render(<App />);
   const button = screen.getByRole('button', { name: 'Change to blue' });
 
   expect(button).toHaveStyle({"background-color": "red"});
+
+  fireEvent.click(button);
+  expect(button).toHaveStyle({ backgroundColor: 'blue' });
+
+  expect(button).toHaveTextContent("Change to red");
 });
 
-test('button has correct initial text', () => {
-  render(<App />);
-  const button = screen.getByRole('button', { name: 'Change to blue' });
-  expect(button.textContent).toBe('Change to blue');
-});
 
-// test('button turns blue when clicked', () => {
-//   render(<App />);
-//   const button = screen.getByRole('button', { name: 'Change to blue' })
-//   button.click()
-//   expect(button).toHaveStyle({backgroundColor: 'blue'})
-// });
 
-// test('button change text when clicked', () => {
-//   render(<App />);
-//   const button = screen.getByRole('button', { name: "Change to red" })
-//   expect(button.textContent).toBe("Change to red")
-// })
