c02b2796ab2445c8e0a274529e18b447a6a68035
CarlosVinicius3258 <carlosvinicius3258@outlook.com>
2023-05-29 23:51:31 -0300

    feat: create button

--- color-button/src/App.tsx
+++ color-button/src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [buttonColor, setButtonColor] = useState<string>('red');
+  const newButtonColor = buttonColor === 'red' ? 'blue' : 'red';
   return (
-    <div style={ { display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px', 
-}}>
-      <button style={ { backgroundColor: 'red', color: 'white', width: 200, height: 40, } } >Change to blue</button>
+    <div style={ {
+      display: 'flex', justifyContent: 'center', alignItems: 'center', padding: '20px',
+    } }>
+      <button onClick={ () => setButtonColor(newButtonColor) } style={ { backgroundColor: buttonColor, color: 'white', width: 200, height: 40, } } >Change to { newButtonColor }</button>
     </div>
 
   );
